cfbb73c92416f2b7e58b7bf3943d378a8c4ffdc1
bjoern <b.jueliger@googlemail.com>
2019-03-23 21:00:04 +0100

    Complete Greenfield, the second

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-mod character;
-mod attributes;
-mod weapons;
-mod data_store;
-
-extern crate serde_derive;
-extern crate serde;
-extern crate serde_json;
-
 fn main() {
     println!("Hello D&D!");
 }
