0dfa0b3749aa3e7eb27dc2d5ad99e64f0788c259
bjoern <b.jueliger@googlemail.com>
2019-03-23 21:39:46 +0100

    Introduce ability scores
    The character now has ability scores that can be set and whose modifier
    can be computed.

--- src/character.rs
+++ src/character.rs
@@ -1,8 +1,10 @@
 use crate::datastore::Datastore;
+use std::collections::HashMap;
 
 pub struct Character<'d> {
     pub name : String,
     data : &'d Datastore,
+    abilities : Abilities,
 }
 
 impl<'d> Character<'d> {
@@ -13,7 +15,57 @@ impl<'d> Character<'d> {
         Character {
             name : String::new(),
             data : data,
+            abilities : Abilities::new(),
         }
+    }
+    /// Returns the current ability score of the character for the ability
+    pub fn ability(&self, ability : &Ability) -> &AbilityScore {
+        self.abilities.get(ability)
+    }
+    pub fn set_ability(&mut self, ability : &Ability, score : AbilityScore) {
+        self.abilities.set(&ability, score);
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
+pub enum Ability { Str, Dex, Con, Wis, Int, Cha }
+type AbilityScore = i8; //Not unsigned because otherwise mismatching types make computing the ability modifier hell
+type Modifier = i8;
+
+struct Abilities {
+    ability_values : HashMap<Ability, AbilityScore>,
+}
+
+use std::iter::{ FromIterator, repeat };
+
+impl Abilities {
+    fn new() -> Self {
+        Abilities {
+            ability_values : HashMap::from_iter(
+                Iterator::zip(
+                    vec![Ability::Str, Ability::Dex, Ability::Con, Ability::Wis, Ability::Int, Ability::Cha].iter().cloned(),
+                    repeat(10).take(6)
+                )
+            )       
+        }
+    }
+    /// This function can safely not deliver an &Option<AbilityValue> because
+    /// the initialization in Abilities::new guarantees that every Ability always has
+    /// an entry in the HashMap self.ability_values
+    fn get(&self, ability : &Ability) -> &AbilityScore {
+        self.ability_values.get(ability).unwrap()
+    }
+    fn set(&mut self, ability : &Ability, value : AbilityScore) {
+        *self.ability_values.get_mut(ability).unwrap() = value;
+    }
+}
+
+impl Ability {
+    /// Returns the ability modifier corresponding to the ability score
+    pub fn score_to_mod(ability_score : &AbilityScore) -> Modifier {
+        let double_mod = ability_score - 10;
+        // Since Rust rounds towards zero, we need to roll our own floor function here
+        if double_mod > 0 { double_mod / 2 } else { ( double_mod - 1 ) / 2 }
     }
 }
 
--- src/character/test_character.rs
+++ src/character/test_character.rs
@@ -7,4 +7,19 @@ mod test_non_data_dependent_features {
         let mut ch = Character::new(&data);
         ch.name = "Dude".to_owned();
     }
+    #[test]
+    fn test_character_abilities_are_initially_10() {
+        let data = Datastore::new();
+        let ch = Character::new(&data);
+        assert_eq!(*ch.ability(&Ability::Str), 10);
+        assert_eq!(Ability::score_to_mod(ch.ability(&Ability::Con)), 0);
+    }
+    #[test]
+    fn test_setting_character_abilities() {
+        let data = Datastore::new();
+        let mut ch = Character::new(&data);
+        ch.set_ability(&Ability::Int, 13);
+        assert_eq!(*ch.ability(&Ability::Int), 13);
+        assert_eq!(Ability::score_to_mod(ch.ability(&Ability::Int)), 1);
+    }
 }
